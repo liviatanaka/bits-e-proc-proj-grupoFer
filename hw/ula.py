e5fca7503c925dd909ce95872b391078bdec08e7
Rafael Corsi <corsiferrao@gmail.com>
2022-09-13 09:47:58 -0300

    ula add conceito A

--- hw/ula.py
+++ hw/ula.py
@@ -129,3 +129,18 @@ def addcla16(a, b, q):
         pass
 
     return instances()
+
+
+# ----------------------------------------------
+# Conceito A
+# ----------------------------------------------
+
+
+@block
+def ula_new(x, y, c, zr, ng, sr, sf, bcd, saida, width=16):
+    pass
+
+
+@block
+def bcdAdder(x, y, z):
+    pass
