b8790b37ac26266ee0e95c964c20923485b2b07a
alanmath <alanmatheus187@gmail.com>
2022-09-15 13:41:47 -0300

    erro addcla4

--- hw/ula.py
+++ hw/ula.py
@@ -63,7 +63,7 @@ def zerador(z, a, y):
 def add(a, b, q):
     @always_comb
     def comb():
-        pass
+        q.next = a+b
 
     return instances()
 
@@ -101,26 +101,70 @@ def halfAdder(a, b, soma, carry):
 @block
 def fullAdder(a, b, c, soma, carry):
     s = [Signal(bool(0)) for i in range(3)]
-    haList = [None for i in range(2)]
+    haList = [None for i in range(2)]  
 
-    haList[0] = halfAdder(a, b, s[0], s[1])  # 2
-    haList[1] = halfAdder(c, s[0], soma, s[2])  # 3
+
+    haList[0] = halfAdder(a, b, s[0], s[1]) 
+    haList[1] = halfAdder(c, s[0], soma, s[2])
 
     @always_comb
     def comb():
-        carry.next = s[1] | s[2]  # 4
+        carry.next = s[1] | s[2]
 
     return instances()
 
+@block
+def adder(x, y, soma, carry):
+    n = len(x)
+    temp = [Signal(bool(0)) for i in range(n)]
+    faList = [None for i in range(n)]
+    faList[0] = halfAdder(x[0],y[0],soma[0], temp[0])
+    for i in range(1,n):
+        faList[i] = fullAdder(x[i], y[i], temp[i-1], soma[i],temp[i])
+
+
+    @always_comb
+    def comb():
+        carry.next = temp[n-1]
+        faList = [None for i in range(4)]
+
+    return instances()
 
 @block
 def addcla4(a, b, q):
+    a_ = [a(i) for i in range(4)]
+    b_ = [b(i) for i in range(4)]
+   
+    temp_carry = [Signal(bool(0)) for i in range(5)]
+    
+    k_list = [Signal(bool(0)) for i in range(4)]
+    p_list = [Signal(bool(0)) for i in range(4)]
+    g_list = [Signal(bool(0)) for i in range(4)]
+    faList = [None for i in range(4)] 
+    guarda_carry_full = [None for i in range(4)]
+    
+    for i in range(4):
+        faList[i] = fullAdder(a_[i], b_[i], temp_carry[i], q[i], guarda_carry_full[i])
+
     @always_comb
     def comb():
-        pass
+        # q.next = a+b
+        for i in range(4):
+            k_list[i] = (not a_[i]) and (not b_[i])
+            g_list[i] = a_[i] and b_[i]
+            p_list[i] = ((not a_[i]) and b_[i]) and ((not b_[i]) and a_[i])
+
+        temp_carry[0] = 0
+        temp_carry[1] = g_list[0] or (p_list[0] and temp_carry[0])
+        temp_carry[2] = g_list[1] or (p_list[1] and g_list[0]) or (p_list[1] and p_list[0] and temp_carry[0])
+        temp_carry[3] = g_list[2] or (p_list[2] and g_list[1]) or (p_list[2] and p_list[1] and g_list[0]) or (p_list[2] and p_list[1] and p_list[0] and temp_carry[0])
+        temp_carry[4] = g_list[3] or (p_list[3] and g_list[2]) or (p_list[3] and p_list[2] and g_list[1]) or (p_list[3] and p_list[2] and p_list[1] and g_list[0]) or (p_list[3] and p_list[2] and p_list[1] and p_list[0] and temp_carry[0])
+
+        
+
+
 
     return instances()
-
 
 @block
 def addcla16(a, b, q):
