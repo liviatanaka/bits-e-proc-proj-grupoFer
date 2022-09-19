4e66682aace5897fecd67df2e0cc53dde79a98f1
fernandovs4 <fervsantos3000@gmail.com>
2022-09-18 23:15:58 -0300

    inversor feito

--- hw/test_ula.py
+++ hw/test_ula.py
@@ -7,7 +7,6 @@ random.seed(5)
 randrange = random.randrange
 
 
-@block
 def test_ula():
     x = Signal(intbv(1)[16:])
     y = Signal(intbv(2)[16:])
@@ -21,67 +20,55 @@ def test_ula():
     def stimulus():
         control.next = 0b000010
         yield delay(10)
-        if saida != x + y:
-            print("erro 1")
+        assert saida == x + y
 
         control.next = 0b000000
         yield delay(10)
-        if saida != (x & y):
-            print("erro 2")
+        assert saida == (x & y)
 
         control.next = 0b100010
         yield delay(10)
-        if saida != y:
-            print("erro 3")
+        assert saida == y
 
         control.next = 0b001010
         yield delay(10)
-        if saida != x:
-            print("erro 4")
+        assert saida == x
 
         control.next = 0b100110
         yield delay(10)
-        if saida != ~y:
-            print("erro 5")
+        assert saida == ~y
 
         control.next = 0b011010
         yield delay(10)
-        if saida != ~x:
-            print("erro 6")
+        assert saida == ~x
 
         control.next = 0b101010
         yield delay(10)
-        if saida != 0:
-            print("erro 7")
+        assert saida == 0
 
         control.next = 0b101000
         yield delay(10)
-        if saida != 0:
-            print("erro 8")
+        assert saida == 0
 
         control.next = 0b101001
         yield delay(10)
-        if saida != intbv(-1)[16:]:
-            print("erro 9")
+        assert saida == intbv(-1)[16:]
 
         # ------ zr ng --------#
-        if zr != 0 and ng != 1:
-            print("erro 10")
+        assert zr == 0 and ng == 1
 
         control.next = 0b101000
         yield delay(10)
-        if zr != 1 and ng != 0:
-            print("erro 11")
+        assert zr == 1 and ng == 0
 
         control.next = 0b000010
         yield delay(10)
-        if zr != 0 and ng != 0:
-            print("erro 12")
+        assert zr == 0 and ng == 0
 
-    return ula_1, stimulus
+    sim = Simulation(ula_1, stimulus)
+    sim.run()
 
 
-@block
 def test_zerador():
     z = Signal(bool(0))
     a = Signal(intbv(0))
@@ -93,19 +80,15 @@ def test_zerador():
         a.next = randrange(2**16 - 1)
         z.next = 0
         yield delay(10)
-        if y != a:
-            print("erro 1")
-            print("%s %s %s" % (bin(z, 1), bin(a, 16), bin(y, 16)))
+        assert y == a
         z.next = 1
         yield delay(10)
-        if y != 0:
-            print("erro 2")
-            print("%s %s %s" % (bin(z, 1), bin(a, 16), bin(y, 16)))
+        assert y == 0
 
-    return zerador_1, stimulus
+    sim = Simulation(zerador_1, stimulus)
+    sim.run()
 
 
-@block
 def test_comparador():
     a = Signal(intbv(0))
     ng = Signal(bool(0))
@@ -116,24 +99,18 @@ def test_comparador():
     def stimulus():
         a.next = 0
         yield delay(10)
-        if ng != 0 or zr != 1:
-            print("erro 1")
-            print("%s %s %s" % (bin(a, 16), bin(zr, 1), bin(ng, 1)))
+        assert ng == 0 or zr == 1
         a.next = 0xFFFF
         yield delay(10)
-        if ng != 1 or zr != 0:
-            print("erro 2")
-            print("%s %s %s" % (bin(a, 16), bin(zr, 1), bin(ng, 1)))
+        assert ng == 1 or zr == 0
         a.next = 32
         yield delay(10)
-        if ng != 0 or zr != 0:
-            print("erro 3")
-            print("%s %s %s" % (bin(a, 16), bin(zr, 1), bin(ng, 1)))
+        assert ng == 0 or zr == 0
 
-    return comparador_1, stimulus
+    sim = Simulation(comparador_1, stimulus)
+    sim.run()
 
 
-@block
 def test_inversor():
     z = Signal(bool(0))
     a = Signal(intbv(0))
@@ -146,22 +123,16 @@ def test_inversor():
         for i in range(256):
             a.next = randrange(2**16 - 1)
             z.next = randrange(2)
-            yield delay(10)
+            yield delay(1)
             if z == 0:
-                if a != y:
-                    print("erro")
-                    print("%s %s %s" % (bin(z, 1), bin(a, 16), bin(y, 16)))
-                    break
+                assert a == y
             else:
-                if a != ~y:
-                    print("erro")
-                    print("%s %s %s" % (bin(z, 1), bin(a, 16), bin(y, 16)))
-                    break
+                assert a == ~y
 
-    return inversor_1, stimulus
+    sim = Simulation(inversor_1, stimulus)
+    sim.run()
 
 
-@block
 def test_inc():
     a = Signal(intbv(0))
     q = Signal(intbv(0))
@@ -173,16 +144,12 @@ def test_inc():
         for i in range(256):
             a.next = randrange(2**16 - 2)
             yield delay(1)
-            if q != a + 1:
-                print("erro")
-                print("%s %s" % (a, q))
-                print("%s %s" % (bin(a, 16), bin(q, 16)))
-                break
+            assert q == a + 1
 
-    return inc16_1, stimulus
+    sim = Simulation(inc16_1, stimulus)
+    sim.run()
 
 
-@block
 def test_add():
     a = Signal(intbv(0))
     b = Signal(intbv(0))
@@ -195,11 +162,7 @@ def test_add():
         for i in range(256):
             a.next, b.next = [randrange(2**15 - 1) for i in range(2)]
             yield delay(1)
-            if q != a + b:
-                print("erro")
-                print("%s %s %s" % (a, b, q))
-                print("%s %s %s" % (bin(a, 16), bin(b, 16), bin(q, 16)))
-                print("%s" % (bin(a + b, 16)))
-                break
+            assert q == a + b
 
-    return add16_1, stimulus
+    sim = Simulation(add16_1, stimulus)
+    sim.run()
--- hw/ula.py
+++ hw/ula.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
+from operator import concat
 from signal import Signals
 from myhdl import *
-
 
 @block
 def ula(x, y, c, zr, ng, saida, width=16):
@@ -23,39 +23,12 @@ def ula(x, y, c, zr, ng, saida, width=16):
     c_f = c(1)
     c_no = c(0)
 
-    # zeradores
-    zerador_x = zerador(c_zx, x, zx_out)
-    zerador_y = zerador(c_zy, y,zy_out)
-
-    # inversores
-    inversor_x = inversor(c_nx, zx_out, nx_out)
-    inversor_y = inversor(c_ny, zy_out, ny_out)
-
-    breakpoint()
-
-    # mux
-    print(f'nx-{nx_out}, ny{ny_out}' )
-    adicao = add(nx_out, ny_out, add_out)
-    
-    print(f'nx-{nx_out}, ny{ny_out}, {add_out}' )
-    xandy = x_and_y(nx_out, ny_out, and_out)
-    mux = mux2way(mux_out, and_out, add_out, c_f)
-    # def mux2way(q, a, b, sel):
-
-
-    # inversor final
-    inversor_final = inversor(c_no, mux_out, no_out)
-
-    #comparador
-    comparador_ = comparador(no_out, zr, ng, width)
-
-    
     @always_comb
     def comb():
-        saida.next = no_out
-        
+        ...
 
     return instances()
+
 
 
 # -z faz complemento de dois
@@ -80,16 +53,7 @@ def comparador(a, zr, ng, width):
     # width insica o tamanho do vetor a
     @always_comb
     def comb():
-        if a == 0:
-            zr.next = 1
-            ng.next = 0
-        elif a < 0:
-            zr.next = 0
-            ng.next = 1
-        else:
-            zr.next = 0
-            ng.next = 0
-            
+        ...
     return instances()
 
 
@@ -97,31 +61,26 @@ def comparador(a, zr, ng, width):
 def zerador(z, a, y):
     @always_comb
     def comb():
-        if z:
-            y.next = a
-        else:
-            y.next = 0
-        
+        ...
 
     return instances()
 
 
 @block
 def add(a, b, q):
+    
     @always_comb
     def comb():
-
-        q.next = a or b
-
+        ...
     return instances()
 
 
 @block
 def inc(a, q):
+    
     @always_comb
     def comb():
-        q.next = add(1, a)
-
+        ...
     return instances()
 
 
@@ -132,8 +91,8 @@ def inc(a, q):
 
 @block
 def halfAdder(a, b, soma, carry):
-    s = Signal(bool())
-    c = Signal(bool())
+    s = Signal(bool(0))
+    c = Signal(bool(0))
 
     @always_comb
     def comb():
@@ -165,7 +124,7 @@ def fullAdder(a, b, c, soma, carry):
 def addcla4(a, b, q):
     @always_comb
     def comb():
-        pass
+        ...
 
     return instances()
 
